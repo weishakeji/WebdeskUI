65c37aff7a070bc5a208bda218155e6d2b53eaa6
weishakeji <10522779@qq.com>
2020-01-12 21:22:48 +0800

    新增:控件的集合管理，常用状态不再直接写在html对象上

--- Scripts/pagebox.js
+++ Scripts/pagebox.js
@@ -24,21 +24,24 @@
         }
         //初始化
         this.init = function() {
-                //如果位置没有设置
-                if (!this.top) {
-                    this.top = (document.documentElement.clientHeight - document.body.scrollTop - this.height) / 2;
-                }
-                if (!this.left) {
-                    this.left = (document.documentElement.clientWidth - document.body.scrollLeft - this.width) / 2;
-                }
-                //设置层深
-                var maxlevel = this.method.maxlevel();
-                this.level = maxlevel < 1 ? 10000 : maxlevel + 1;
-                //默认图标
-                if (this.ico == null) this.ico = '&#174;';
-
+            this.id = 'pagebox_' + new Date().getTime();
+            //如果位置没有设置
+            if (!this.top) {
+                this.top = (document.documentElement.clientHeight - document.body.scrollTop - this.height) / 2;
             }
-            //方法
+            if (!this.left) {
+                this.left = (document.documentElement.clientWidth - document.body.scrollLeft - this.width) / 2;
+            }
+            //设置层深
+            var maxlevel = this.method.maxlevel();
+            this.level = maxlevel < 1 ? 10000 : maxlevel + 1;
+            //默认图标
+            if (this.ico == null) this.ico = '&#174;';
+            $ctrls.add({ id: this.id, obj: this, type: 'pagebox' });
+            return this;
+        };
+
+        //方法
         this.method = {
                 //获取所有pagebox窗体元素
                 getboxs: function() {
@@ -57,17 +60,17 @@
                 pagebox.focus(this.id);
                 return;
             }
-            this.init();
             //创建窗体
             for (var t in this.builder) {
                 this.builder[t](this);
             }
-            //添加事件
-            var boxele = document.querySelector('.pagebox[boxid=\'' + this.id + '\']');
+            //添加事件（基础事件，例如移动、拖放等，并不包括自定义事件）
+            boxele = document.querySelector('.pagebox[boxid=\'' + this.id + '\']');
             for (var t in this.event) {
                 this.event[t](boxele);
             }
             this.focus();
+            $ctrls.update({ id: this.id, dom: $dom(boxele) });
             return this;
         };
         //设置当前窗体为焦点
@@ -79,18 +82,12 @@
         };
         //构建pagebox窗体
         this.builder = {
-                //生成随机id
-                randomid: function(box) {
-                    box.id = 'pagebox_' + new Date().getTime();
-                },
                 //生成外壳
                 shell: function(box) {
                     var div = $dom(document.body).append('div').childs().last();
                     div.attr({
                         'boxid': box.id,
-                        'class': 'pagebox',
-                        'ismove': box.move,
-                        'isresize': box.resize
+                        'class': 'pagebox'
                     });
                     div.css({
                         'top': box.top + 'px',
@@ -105,7 +102,7 @@
                     var arr = ['nw', 'w', 'sw', 'n', 's', 'ne', 'e', 'se'];
                     for (var i = 0; i < arr.length; i++) {
                         var node = margin.append(arr[i]).find(arr[i]);
-                        if (box.isresize)
+                        if (box.resize)
                             node.css('cursor', arr[i] + '-resize');
                     }
                 },
@@ -153,48 +150,27 @@
                     pagebox.focus(boxid);
                 });
             },
-            title_click: function(box) {
-                $dom(box).find('pagebox_title').click(function(event) {
-                    var node = event.target ? event.target : event.srcElement;
-                    while (!node.getAttribute('boxid')) node = node.parentNode;
-                    console.log('点击标题：' + node.innerText);
-                    event.stopPropagation();
-                });
-            },
-            title_dbClick: function(box) {
-                $dom(box).find('pagebox_title').dblclick(function(event) {
-                    var node = event.target ? event.target : event.srcElement;
-                    while (!node.getAttribute('boxid')) node = node.parentNode;
-
-                    //event.stopPropagation();
-                });
-            },
             //拖动事件的起始，当鼠标点下时
             pagebox_drag: function(pageboxElement) {
                 var box = $dom(pageboxElement);
                 var dragbar = box.find('pagebox_dragbar');
                 dragbar = dragbar.merge(box.find('margin>*'));
                 dragbar.mousedown(function(e) {
-                    var mouse = $dom.mouse(e); //鼠标的当前坐标
                     //鼠标点中的对象
                     var node = event.target ? event.target : event.srcElement;
-                    var tagname = node.tagName.toLowerCase(); //点中的节点名称
-                    //console.log(tagname);
+                    var tagname = node.tagName.toLowerCase(); //点中的节点名称                   
                     //获取窗体对象
                     while (!node.getAttribute('boxid')) node = node.parentNode;
-                    var page = $dom(node);
-                    var offset = page.offset();
-                    page.attr('dragtrigger', tagname).attr({
-                        'down_x': mouse.x,
-                        'down_y': mouse.y,
-                        'down_left': offset.left,
-                        'down_top': offset.top,
-                        'down_width': page.width(),
-                        'down_height': page.height()
-                    }).addClass('pagebox_drag');
+                    var ctrl = $ctrls.get(node.getAttribute('boxid'));
+                    //记录当鼠标点下时的数据
+                    var offset = ctrl.dom.offset();
+                    ctrl.dragtrigger = tagname;
+                    ctrl.down_mouse = $dom.mouse(e);
+                    ctrl.down_offset = ctrl.dom.offset();
+                    ctrl.down_size = { width: ctrl.dom.width(), height: ctrl.dom.height() };
+                    ctrl.dom.addClass('pagebox_drag');
                     //设置当前窗体为焦点窗
-                    var boxid = page.attr('boxid');
-                    pagebox.focus(boxid);
+                    pagebox.focus(ctrl.id);
                 });
                 //dragbar.addEventListener('mousedown', );
             },
@@ -207,27 +183,35 @@
                     while (!node.getAttribute('boxid')) node = node.parentNode;
                     box.close(node);
                 });
-                //最大化
+                //最大化或还原
                 boxdom.find('btnbox btn_max').click(function(e) {
                     var node = event.target ? event.target : event.srcElement;
                     while (!node.getAttribute('boxid')) node = node.parentNode;
-                    if ($dom(node).hasClass('pagebox_full')) box.toWindow(node);
+                    var boxid = node.getAttribute('boxid');
+                    if ($dom(node).hasClass('pagebox_full')) box.toWindow(boxid);
                     else
-                        box.toFull(node);
+                        box.toFull(boxid);
                 });
+                //双击标题栏，最大化或还原
                 boxdom.find('pagebox_dragbar').dblclick(function(e) {
                     var node = event.target ? event.target : event.srcElement;
                     while (!node.getAttribute('boxid')) node = node.parentNode;
-                    if ($dom(node).hasClass('pagebox_full')) box.toWindow(node);
+                    var boxid = node.getAttribute('boxid');
+                    if ($dom(node).hasClass('pagebox_full')) box.toWindow(boxid);
                     else
-                        box.toFull(node);
+                        box.toFull(boxid);
                 });
 
             }
-        }
+        };
+        this.init();
     };
     //*** 以下是静态方法 */
-    //创建并打开一个窗体
+    //他对一个窗体对象
+    box.create = function(param) {
+            return new box(param);
+        }
+        //创建窗体对象并打开
     box.open = function(param) {
         return new box(param).open();
     };
@@ -267,19 +251,19 @@
     };
     //最大化
     box.toFull = function(boxid) {
-        var page = box.dom(boxid);
-        //禁止移动和调整大小
-        page.attr('move', 'false').attr('resize', 'false');
-        var offset = page.offset();
-        page.attr({
-            'win_left': offset.left,
-            'win_top': offset.top,
-            'win_width': page.width(),
-            'win_height': page.height()
-        }).addClass('pagebox_full');
-        page.css('transition', 'width 0.3s,height 0.3s,left 0.3s,top 0.3s');
-        page.width(window.innerWidth - 3).height(innerHeight - 2).left(1).top(0);
-        box.disableResize(page);
+        var ctrl = $ctrls.get(boxid);
+        //记录放大前的数据，用于还原
+        ctrl.win_offset = ctrl.dom.offset();
+        ctrl.win_size = { width: ctrl.dom.width(), height: ctrl.dom.height() };
+        ctrl.win_state = { move: ctrl.obj.move, resize: ctrl.obj.resize };
+        ctrl.obj.move = ctrl.obj.resize = false;
+        //开始全屏放大        
+        ctrl.dom.css('transition', 'width 0.3s,height 0.3s,left 0.3s,top 0.3s').addClass('pagebox_full');
+        ctrl.dom.width(window.innerWidth - 3).height(innerHeight - 2).left(1).top(0);
+        //禁用缩放样式（鼠标手势）
+        ctrl.dom.find('margin>*').each(function() {
+            $dom(this).css('cursor', 'default');
+        });
     };
     //最小化
     box.toMinimize = function(boxid) {
@@ -287,62 +271,61 @@
     };
     //恢复窗体状态
     box.toWindow = function(boxid) {
-        var page = box.dom(boxid);
-        //原始坐标与宽高
-        var dl = parseFloat(page.attr('win_left'));
-        var dt = parseFloat(page.attr('win_top'));
-        var dw = parseFloat(page.attr('win_width'));
-        var dh = parseFloat(page.attr('win_height'));
-        //
-        page.width(dw).height(dh).left(dl).top(dt);
+        var ctrl = $ctrls.get(boxid);
+        ctrl.dom.width(ctrl.win_size.width).height(ctrl.win_size.height)
+            .left(ctrl.win_offset.left).top(ctrl.win_offset.top);
+        ctrl.obj.move = ctrl.win_state.move;
+        ctrl.obj.resize = ctrl.win_state.resize;
         window.setTimeout(function() {
-            page.css('transition', '');
+            ctrl.dom.css('transition', '');
         }, 300);
-        page.removeAttr('resize').removeAttr('move').removeClass('pagebox_full');
-        box.enableResize(page);
+        ctrl.dom.removeClass('pagebox_full');
+        //恢复缩放窗体的鼠标手势
+        if (ctrl.obj.resize) {
+            ctrl.dom.find('margin>*').each(function() {
+                $dom(this).css('cursor', this.tagName + '-resize');
+            });
+        }
     };
     //禁用缩放
     box.disableResize = function(boxid) {
-        var page = box.dom(boxid);
-        page.attr('isresize', false).find('margin>*').each(function() {
+        var ctrl = $ctrls.get(boxid);
+        ctrl.dom.find('margin>*').each(function() {
             $dom(this).css('cursor', 'default');
         });
     };
     //启用缩放
     box.enableResize = function(boxid) {
-        var page = box.dom(boxid);
-        page.attr('isresize', true).find('margin>*').each(function() {
+        var ctrl = $ctrls.get(boxid);
+        ctrl.dom.find('margin>*').each(function() {
             $dom(this).css('cursor', this.tagName + '-resize');
         });
     };
     //拖动窗体所需的事件
     box.dragRealize = function() {
         document.addEventListener('mousemove', function(e) {
-            var box = $dom('div[dragtrigger].pagebox_focus');
+            var box = $dom('div.pagebox_drag');
             if (box.length < 1) return;
-            //当鼠标的历史坐标
-            var dx = parseFloat(box.attr('down_x'));
-            var dy = parseFloat(box.attr('down_y'));
-            //当鼠标点下时的pagebox坐标
-            var dl = parseFloat(box.attr('down_left'));
-            var dt = parseFloat(box.attr('down_top'));
-            var dw = parseFloat(box.attr('down_width'));
-            var dh = parseFloat(box.attr('down_height'));
+            var ctrl = $ctrls.get(box.attr('boxid'));
+            //当鼠标点下时的历史坐标            
+            var dl = ctrl.down_offset.left;
+            var dt = ctrl.down_offset.top;
+            var dw = ctrl.down_size.width;
+            var dh = ctrl.down_size.height;
             //获取移动距离
             var mouse = $dom.mouse(e);
             mouse.x = mouse.x < 0 ? 0 : (mouse.x > window.innerWidth ? window.innerWidth : mouse.x);
             mouse.y = mouse.y < 0 ? 0 : (mouse.y > window.innerHeight ? window.innerHeight : mouse.y);
-            var movex = mouse.x - dx;
-            var movey = mouse.y - dy;
+            var movex = mouse.x - ctrl.down_mouse.x;
+            var movey = mouse.y - ctrl.down_mouse.y;
             //触发拖动的对象，即当鼠标点下时，点中的对象
-            var target = box.attr('dragtrigger');
+            var target = ctrl.dragtrigger;
             //移动窗体   
-            if (target == 'pagebox_dragbar' && box.attr('move') != 'false') {
-                if (box.attr('ismove') == 'true')
-                    box.left(dl + movex).top(dt + movey);
+            if (target == 'pagebox_dragbar') {
+                if (ctrl.obj.move) box.left(dl + movex).top(dt + movey);
             } else {
                 //缩放窗体
-                if (box.attr('isresize') == 'true') {
+                if (ctrl.obj.resize) {
                     var minWidth = 200,
                         minHeight = 150;
                     if (box.attr('resize') != 'false') {
@@ -362,13 +345,6 @@
             window.msg = '移动：x_' + movex + ",y_" + movey + ',pagebox宽度：' + box.width();
         });
         document.addEventListener('mouseup', function(e) {
-            //删除拖动时生成的一些临时属性
-            var rmattr = ['drag', 'down_x', 'down_y', 'down_left', 'down_top', 'down_width', 'down_height'];
-            $dom('.pagebox').each(function() {
-                for (var i = 0; i < rmattr.length; i++) {
-                    this.removeAttribute(rmattr[i]);
-                }
-            });
             $dom('.pagebox_focus').removeClass('pagebox_drag');
         });
         window.addEventListener('blur', function(e) {
